6933e2e89a8285dcfd9c92313065fcd620fca587
Святослав Поляков <viewtifulmadguy@gmail.com>
2024-05-25 16:46:03 +0300

    Update urls

--- core/urls.py
+++ core/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('@<username>/', profile_view, name='profile')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
